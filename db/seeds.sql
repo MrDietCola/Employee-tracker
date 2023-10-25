61a2849daaa3b19c70fa2b39add2a3215fd21f6e
AJ Seidler <ajseidler0526@gmail.com>
2023-10-24 23:49:19 -0500

    fixed seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,53 +10,29 @@ VALUES
   ('HR Manager', 75000, 1),
   ('Software Engineer', 80000, 2),
   ('Marketing Manager', 70000, 3),
-  ('Financial Analyst', 65000, 4);
+  ('Financial Analyst', 65000, 4),
   ('Graphic Designer', 60000, 3),
-  ('Sales Associate', 45000, 5),
+  ('Sales Associate', 45000, 2),
   ('Human Resources Specialist', 70000, 1),
-  ('Customer Support Representative', 40000, 6),
+  ('Customer Support Rep', 40000, 1),
   ('Data Scientist', 110000, 2),
-  ('Project Manager', 90000, 7),
-  ('Senior Software Engineer', 120000, 2),
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES
-  ('John', 'Smith', 1, NULL),
-  ('Sarah', 'Johnson', 2, NULL),
-  ('Michael', 'Brown', 3, 1),
-  ('Lisa', 'Davis', 4, 1),
-  ('William', 'Anderson', 5, 2),
-  ('Jessica', 'Lee', 6, 2),
-  ('David', 'Martin', 3, 1),
-  ('Jennifer', 'Harris', 7, 2),
-  ('James', 'White', 3, 1),
-  ('Susan', 'Hall', 8, 3),
-  ('Joseph', 'King', 9, 3),
-  ('Linda', 'Turner', 4, 1),  
-  ('Thomas', 'Wright', 5, 2),
-  ('Maria', 'Baker', 6, 2);
-
-INSERT INTO role (title, salary, department_id)
-VALUES
-  ('Graphic Designer', 60000, 3),
-  ('Sales Associate', 45000, 5),
-  ('Human Resources Specialist', 70000, 1),
-  ('Customer Support Rep', 40000, 6),
-  ('Data Scientist', 110000, 2),
-  ('Project Manager', 90000, 7),
+  ('Project Manager', 90000, 1),
   ('Senior Software Engineer', 120000, 2);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES   
+  ('Michael', 'Brown', 3, null),
+  ('Lisa', 'Davis', 4, null),
+  ('William', 'Anderson', 5, null),
+  ('Jessica', 'Lee', 5, null),
+  ('David', 'Martin', 3, null),
+  ('Jennifer', 'Harris', 2, null);
 
-  INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES   ('Michael', 'Brown', 3, 1),
-  ('Lisa', 'Davis', 4, 1),
-  ('William', 'Anderson', 5, 2),
-  ('Jessica', 'Lee', 6, 2),
-  ('David', 'Martin', 3, 1),
-  ('Jennifer', 'Harris', 7, 2),
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  
   ('James', 'White', 3, 1),
-  ('Susan', 'Hall', 8, 3),
-  ('Joseph', 'King', 9, 3),
+  ('Susan', 'Hall', 5, 3),
+  ('Joseph', 'King', 5, 3),
   ('Linda', 'Turner', 4, 1),  
   ('Thomas', 'Wright', 5, 2),
   ('Maria', 'Baker', 6, 2);
